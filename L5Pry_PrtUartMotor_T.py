9fa283a2a08646ead53e3e86b8fa8301d2995f0c
Jorge Salgueiro <yorch.academico@gmail.com>
2025-10-03 20:52:45 -0400

    Agregado comentarios

--- L5Pry_PrtUartMotor_T.py
+++ L5Pry_PrtUartMotor_T.py
@@ -1,39 +1,52 @@
-import RPi.GPIO as GPIO
-import serial
-from time import sleep
+# Librerías:
+import RPi.GPIO as GPIO # GPIO
+import serial # Comunicación Serial
+from time import sleep # Retardo
 
-BtnBuzzer = 5
+# Variables:
+BtnBuzzer = 5 # Botón Buzzer
+#----------------------{ Motores PWM
 PwmMtrB = 6 
 PwmMtrA = 26
 HighMtrAB = 13 
 LowMtrAB = 19
 estM1 = estM2 = False
+#}--
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(BtnBuzzer, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Configuración:
+GPIO.setmode(GPIO.BCM) # Pines (Modo de numeración BCM)
+GPIO.setup(BtnBuzzer, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Buzzer
+#--------------------------------{ Motores PWM
 GPIO.setup(PwmMtrA, GPIO.OUT)
 GPIO.setup(PwmMtrB, GPIO.OUT)
 GPIO.setup(HighMtrAB, GPIO.OUT)
 GPIO.setup(LowMtrAB, GPIO.OUT)
+
+# Configuración PWM: 
 pwmA = GPIO.PWM(PwmMtrA, 1000)
 pwmB = GPIO.PWM(PwmMtrB, 1000)
 pwmA.start(0)
 pwmB.start(0)
 
+# Configuración UART:
 ser = serial.Serial('/dev/ttyACM0', 9600)
 ser.reset_input_buffer()
 
+# Inicialización:
 GPIO.output(HighMtrAB, 1)
 GPIO.output(LowMtrAB, 0)
 
 while True:
     try:
+        # Al presionar el botón del Buzzer envía el comando "buzzer" por UART: {=>
         if GPIO.input(BtnBuzzer) == GPIO.LOW:
             ser.write(b"buzzer\n")
             print("enviado: buzzer")
             sleep(0.2)
+        # <=} Si recibe datos por UART:{=>
         if ser.in_waiting > 0:
             value = ser.readline().decode('utf-8').rstrip()
+            # Si "motor1" activar - desactivar Motor 1 [=>
             if value == "motor1":
                 print("Recibido1", value)
                 if estM1 == False:
@@ -42,6 +55,7 @@ while True:
                 else:
                     estM1 = False
                     pwmA.ChangeDutyCycle(0)
+            # <=] Si "motor1" activar - desactivar Motor 1 [=>
             if value == "motor2":
                 print("Recibido2", value)
                 if estM2 == False:
@@ -50,7 +64,9 @@ while True:
                 else:
                     estM2 = False
                     pwmB.ChangeDutyCycle(0)
+            # <=]
             else:
                 print(value)
+        # <=}
     except Exception as e:
         print(e)
--- L5Pry_PrtUartMotor_T/L5Pry_PrtUartMotor_T.c
+++ L5Pry_PrtUartMotor_T/L5Pry_PrtUartMotor_T.c
@@ -1,135 +1,191 @@
+/* LEEME
+	Laboratorio 5 - 1er Fase Proyecto
+
+	Hardware - Pines usados:
+	- PJ0: Botón 1 (Señal motor1 - Raspberry)
+	- PJ1: Botón 2 (Señal motor2 - Raspberry)
+	- PN0: LED indicador botón 1 (motor1)
+	- PN1: LED indicador botón 2 (motor2)
+	- PN2: Buzzer
+	- PF0: LED indicador actividad UART
+	- PF4: LED indicador recepción comando UART
+	- PA0: UART0 RX (USB)
+	- PA1: UART0 TX (USB)
+
+	Descripción:
+	Programa para la placa EK-TM4C1294XL que realiza las siguientes funciones:
+	- Lee el estado de dos botones integrados en la placa (PJ0 y PJ1). Al presionar PJ0 envía por UART "motor1"
+		y enciende el LED PN0. Al presionar PJ1 envía por UART "motor2" y enciende el LED PN1.
+	- Escucha comandos por UART. Si recibe "buzzer", enciende el buzzer (PN2) durante 2 segundos.
+	- El LED PF4 indica la actividad del UART (se enciende al recibir un comando).
+	- El LED PF0 parpadea cada vez que se recibe un comando por UART.
+*/
+
+// Librerías:
+//-----------------------------{ Mínimas
 #include <stdint.h>
 #include <stdbool.h>
 #include "inc/hw_memmap.h"
 #include "driverlib/debug.h"
 #include "driverlib/gpio.h"
 #include "driverlib/sysctl.h"
-#include "string.h"
-#include "driverlib/pin_map.h"
-// Uart libraries:
+//}----------------------------{ Uart
 #include "driverlib/uart.h"
 #include "utils/uartstdio.c"
-//-------------------------------
+//}----------------------------{ Adicionales
+#include "string.h"
+#include "driverlib/pin_map.h"
+//}--
 
+// ======================================================
+
+// Variables globales:
 int freq = 120000000;
 volatile uint32_t ui32Loop;
 
+// Prototipos de funciones:
 void LecBtn();
 void checkUART(char rxBuffer[10], int *rxIndex);
 void interactiveDelay(float time_sec, int *tIter);
+void toogleUart(bool *ledUart);
+
+// =======================================================
 
 int main(void)
 {
-    char rxBuffer[10];
-    bool ledUart = true;
-    int rxIndex = 0;
-    int tIter = 0;
-    bool BuzzerState = false;
-    float time_sec = 2.0;
+	// Variables main:
+	//-------------------------{ Uart 
+	char rxBuffer[10];
+	int rxIndex = 0;
+	bool ledUart = true;
+	//}------------------------{ InteractiveDelay 
+	int tIter = 0;
+	float time_sec = 2.0;
+	//}--
+	bool BuzzerState = false; // Buzzer
 
-    SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ | SYSCTL_OSC_MAIN | SYSCTL_USE_PLL | SYSCTL_CFG_VCO_480),freq); 
+	SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ  // Configuración del reloj de la placa
+						| SYSCTL_OSC_MAIN
+						| SYSCTL_USE_PLL 
+						| SYSCTL_CFG_VCO_480),freq); 
 
-    // Habilitar perifericos:
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
-    //-------------------------------------------
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
-    SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
-    
-    // Verificar perifericos:
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0))  {}
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)) {}
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA))  {}
-    //-----------------------------------------------------
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ))  {}
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))  {}
-    while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))  {}
+	// Habilitar perifericos:
+	//---------------------------------------------{ Uart 
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+	//}--------------------------------------------{ GPIO 
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOJ);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION);
+	SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOF);
+	//}--
 
+	// Verificar perifericos:
+	//-----------------------------------------------------{ Uart 
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_UART0))  {}
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_TIMER0)) {}
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOA))  {}
+	//-----------------------------------------------------{ GPIO 
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOJ))  {}
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION))  {}
+	while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOF))  {}
+	//}--
 
-    GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, 0x07);
-    GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, 0x11);
-    //--------------------------------------------------------------
-    GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, 0x03);
-    GPIOPadConfigSet(GPIO_PORTJ_BASE, 0x03, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
-    //--------------------------------------------------------------
-    // Configurar Uart:
-    GPIOPinConfigure(GPIO_PA0_U0RX);
-    GPIOPinConfigure(GPIO_PA1_U0TX);
-    GPIOPinTypeUART(GPIO_PORTA_BASE, 0x03);
+	// Configurar pines 
+	//--------------------------------------------------------------{ GPIO Outputs
+	GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, 0x07); // PN0,PN1,PN2
+	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, 0x11);	// PF0,PF4
+	//}-------------------------------------------------------------{ GPIO Inputs
+	GPIOPinTypeGPIOInput(GPIO_PORTJ_BASE, 0x03); // PJ0, PJ1
+	GPIOPadConfigSet(GPIO_PORTJ_BASE, 0x03, GPIO_STRENGTH_2MA, GPIO_PIN_TYPE_STD_WPU);
+	//}-------------------------------------------------------------{ Uart
+	GPIOPinConfigure(GPIO_PA0_U0RX); // USB
+	GPIOPinConfigure(GPIO_PA1_U0TX);
+	GPIOPinTypeUART(GPIO_PORTA_BASE, 0x03);
+	//}--
 
-    UARTStdioConfig(0, 9600, freq);
+	UARTStdioConfig(0, 9600, freq);
 
-    while(1)
-    {
-      LecBtn();
-      GPIOPinWrite(GPIO_PORTN_BASE, 0x03, 0);  // Apaga PN0 y PN1
-      checkUART(rxBuffer, &rxIndex);
-      GPIOPinWrite(GPIO_PORTF_BASE, 0x01, 0);
-      if (strncmp(rxBuffer, "buzzer", 6) == 0) {
-        BuzzerState = true;
-        memset(rxBuffer, 0, sizeof(rxBuffer));
-        GPIOPinWrite(GPIO_PORTN_BASE, 0x04, 0x04);
-        if (ledUart == true) {
-          ledUart = false;
-          GPIOPinWrite(GPIO_PORTF_BASE, 0x10, 0);
-        } else {
-          ledUart = true;
-          GPIOPinWrite(GPIO_PORTF_BASE, 0x10, 0x10);
-        }
-      }
-      if (BuzzerState == true) {
-        interactiveDelay(time_sec, &tIter);
-        if (tIter == 0) {
-          BuzzerState = false;
-          GPIOPinWrite(GPIO_PORTN_BASE, 0x04, 0);
-        }
-      }
-    }
+	while(1)
+	{
+		GPIOPinWrite(GPIO_PORTN_BASE, 0x03, 0);  // Apaga PN0 y PN1
+		LecBtn();
+		checkUART(rxBuffer, &rxIndex);
+		GPIOPinWrite(GPIO_PORTF_BASE, 0x01, 0);
+		// Activar Buzzer al recibir por Uart "buzzer" {=>
+		if (strncmp(rxBuffer, "buzzer", 6) == 0) {
+			BuzzerState = true;
+			memset(rxBuffer, 0, sizeof(rxBuffer));
+			GPIOPinWrite(GPIO_PORTN_BASE, 0x04, 0x04);
+			toogleUart(&ledUart);
+		}
+		// <=} Desactivar Buzzer después de 2 segundos {=>
+		if (BuzzerState == true) {
+			interactiveDelay(time_sec, &tIter);
+			if (tIter == 0) {
+				BuzzerState = false;
+				GPIOPinWrite(GPIO_PORTN_BASE, 0x04, 0);
+			}
+		}
+		// <=}
+	}
 }
 
 //============================================================================
 
+// Si se presiona el botón, enviar mensaje por Uart: "motor1" o "motor2"
 void LecBtn() {
-  if ((GPIOPinRead(GPIO_PORTJ_BASE, 0x01) == 0)) {
-    UARTprintf("motor1\n");
-    GPIOPinWrite(GPIO_PORTN_BASE, 0x01, 0x01);
-    for (ui32Loop = 0; ui32Loop < (freq/60); ui32Loop++) {}
-  }
-  if ((GPIOPinRead(GPIO_PORTJ_BASE, 0x02) == 0)) {
-    UARTprintf("motor2\n");
-    GPIOPinWrite(GPIO_PORTN_BASE, 0x02, 0x02);
-    for (ui32Loop = 0; ui32Loop < (freq/60); ui32Loop++) {}
-  }
+	if ((GPIOPinRead(GPIO_PORTJ_BASE, 0x01) == 0)) {
+		UARTprintf("motor1\n");
+		GPIOPinWrite(GPIO_PORTN_BASE, 0x01, 0x01);
+		for (ui32Loop = 0; ui32Loop < (freq/60); ui32Loop++) {}
+	}
+	if ((GPIOPinRead(GPIO_PORTJ_BASE, 0x02) == 0)) {
+		UARTprintf("motor2\n");
+		GPIOPinWrite(GPIO_PORTN_BASE, 0x02, 0x02);
+		for (ui32Loop = 0; ui32Loop < (freq/60); ui32Loop++) {}
+	}
 }
 //------------------------------------------------------------------
+// Verificar si hay datos recibidos por Uart y almacenarlos en rxBuffer
 void checkUART(char rxBuffer[10], int *rxIndex) {
-  int c;
-  while (UARTCharsAvail(UART0_BASE)) {
-    c = UARTCharGetNonBlocking(UART0_BASE);
+	int c;
+	while (UARTCharsAvail(UART0_BASE)) {
+		c = UARTCharGetNonBlocking(UART0_BASE);
 
-    if (c == -1) return;
+		if (c == -1) return;
 
-    if (c == '\r' || c == '\n') {
-      rxBuffer[*rxIndex] = '\0';
-      *rxIndex = 0;
-      GPIOPinWrite(GPIO_PORTF_BASE, 0x01, 0x01);
-      for (ui32Loop = 0; ui32Loop < (freq/60); ui32Loop++) {}
-    } else {
-      if (*rxIndex < 9) {
-        rxBuffer[*rxIndex] = (char)c;
-        (*rxIndex)++;
-      }
-    }
-  }
+		if (c == '\r' || c == '\n') {
+			rxBuffer[*rxIndex] = '\0';
+			*rxIndex = 0;
+			GPIOPinWrite(GPIO_PORTF_BASE, 0x01, 0x01);
+			for (ui32Loop = 0; ui32Loop < (freq/60); ui32Loop++) {}
+		} else {
+			if (*rxIndex < 9) {
+				rxBuffer[*rxIndex] = (char)c;
+				(*rxIndex)++;
+			}
+		}
+	}
 }
 //------------------------------------------------------------------
+// Retardo interactivo que permite ejecutar otras tareas
 void interactiveDelay(float time_sec, int *tIter){
-  int TotalTimeIter = (int)(time_sec*10);
-  if (*tIter == 0) {
-    *tIter = TotalTimeIter;
-  }
-  for (ui32Loop = 0; ui32Loop < (freq/100); ui32Loop++) {}
-  *tIter -= 1;
+	int TotalTimeIter = (int)(time_sec*10);
+	if (*tIter == 0) {
+		*tIter = TotalTimeIter;
+	}
+	for (ui32Loop = 0; ui32Loop < (freq/100); ui32Loop++) {}
+	*tIter -= 1;
+}
+//------------------------------------------------------------------
+// Alternar estado del LED de UART
+void toogleUart(bool *ledUart){
+	if (*ledUart == true) {
+		*ledUart = false;
+		GPIOPinWrite(GPIO_PORTF_BASE, 0x10, 0);
+	} else {
+		*ledUart = true;
+		GPIOPinWrite(GPIO_PORTF_BASE, 0x10, 0x10);
+	}
 }
